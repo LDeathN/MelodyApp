8d59fd7a845f9ddc23799a8f3e22439b0e69d0f4
Martin <martyndaskalov2004@gmail.com>
2025-07-15 19:51:07 +0300

    Added Application User that extends Identity User

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿namespace MelodyApp.Models
+{
+    using Microsoft.AspNetCore.Identity;
+    public class ApplicationUser : IdentityUser
+    {
+        //TODO
+        //public ICollection<FavoriteSong> FavoriteSongs { get; set; } = new List<FavoriteSong>();
+    }
+}
