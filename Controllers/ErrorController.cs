6c0f28a81e3fab6bb9ead6e9e5ac1ad5cb9c7814
Martin <martyndaskalov2004@gmail.com>
2025-08-06 03:00:39 +0300

    Added special pages for 404 and 500 pages

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ErrorController : Controller
+{
+    [Route("Error/404")]
+    public IActionResult NotFoundPage()
+    {
+        return View("~/Views/Shared/Error404.cshtml");
+    }
+
+    [Route("Error/500")]
+    [Route("Error/ServerError")]
+    public IActionResult ServerError()
+    {
+        return View("~/Views/Shared/Error500.cshtml");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -34,8 +34,8 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error/ServerError");
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
     app.UseHsts();
 }
 
